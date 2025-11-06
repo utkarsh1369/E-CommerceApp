c02c632de98f70993806a991e2034ed17ddf1fb2
utkarsh <utkarsh.gupta26095@gmail.com>
2025-11-06 11:05:19 +0530

    added super user registration

---
+++ user-service/src/main/java/com/microservices/user_service/config/AdminSecretProperties.java
@@ -0,0 +1,14 @@
+package com.microservices.user_service.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "admin.secret")
+public class AdminSecretProperties {
+    private String code;
+}
---
+++ user-service/src/main/java/com/microservices/user_service/model/dto/SuccessMessage.java
@@ -0,0 +1,10 @@
+package com.microservices.user_service.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SuccessMessage {
+    private String message;
+}
