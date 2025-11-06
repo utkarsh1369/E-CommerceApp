beb26111338ae83bb2056341e51736fd5e952d81
utkarsh <utkarsh.gupta26095@gmail.com>
2025-11-06 11:04:55 +0530

    added swagger ui

---
+++ user-service/src/main/java/com/microservices/user_service/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.microservices.user_service.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(tags = {
+        @Tag(name = "Authorization APIs"),
+        @Tag(name = "User APIs")
+})
+public class SwaggerConfig {
+
+        @Bean
+        public OpenAPI myCustomConfig() {
+            return new OpenAPI().info(
+                    new Info().title("User Service APIs").description("By Utkarsh")
+            ).components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")
+                                    .in(SecurityScheme.In.HEADER)
+                                    .name("Authorization")));
+        }
+}
