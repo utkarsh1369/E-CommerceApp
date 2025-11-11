d8fae79d50fac3c50519ed2055781c7e49ac0eb2
utkarsh <utkarsh.gupta26095@gmail.com>
2025-11-11 14:06:29 +0530

    added validation

--- user-service/src/main/java/com/microservices/user_service/model/dto/UserDto.java
+++ user-service/src/main/java/com/microservices/user_service/model/dto/UserDto.java
@@ -1,6 +1,9 @@
 package com.microservices.user_service.model.dto;
 
 import com.microservices.user_service.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,10 +17,22 @@ import java.util.Set;
 public class UserDto {
 
     private String userId;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Phone number cannot be blank")
+    @Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 digits")
     private String phoneNumber;
+
+    @NotBlank(message = "Address cannot be blank")
     private String address;
+
     private Set<Role> roles;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
