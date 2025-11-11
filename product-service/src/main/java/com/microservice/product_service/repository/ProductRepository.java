2e18234fa99175e207d102dd365306cd26351560
utkarsh <utkarsh.gupta26095@gmail.com>
2025-11-11 16:51:17 +0530

    added unit tests

--- product-service/src/main/java/com/microservice/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/microservice/product_service/repository/ProductRepository.java
@@ -4,12 +4,6 @@ import com.microservice.product_service.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    Optional<Product> findByProductName(String productName);
-
-    boolean existsByProductName(String productName);
 }
